ab20ea616fe7da5db5ca29e1e9215a7097b01f61
Kourtney Smith <117469291+KourtneySmith@users.noreply.github.com>
2022-11-14 19:27:08 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,28 @@
 // write your code here
 //Kourtney Smith J00951715 Nov/9/2022
-#include <iostream>
-#include <cmath>
-#include <iomanip>
+#include <stdio.h>
+#include<iostream>
 using namespace std;
-
-int main() {
-  int n;
-  
-  cin >> n;
-  if ( n>= 1 && n <= 1000) {
-    if (n==2||n==3||n==5||n==7||n==11||n==13||n==17||n==19||n==23||n==29||n==31||n==47)
-    cout << "Number " << n <<" is prime!"<< endl; }
-  
-
-    if (n%2==0 && n!= 2) {cout <<"2"<< " ";}
-    if (n%3==0 && n!= 3) {cout <<"3"<< " ";}
-    if (n%5==0 && n!= 5) {cout <<"5"<< " ";}
-    if (n%7==0 && n!= 7) {cout <<"7"<< " ";}
-    if (n%11==0 && n!= 11) {cout <<"11"<< " ";}
-    if (n%13==0 && n!= 13) {cout <<"13"<< " ";}
-    if (n%17==0 && n!= 17) {cout <<"17"<< " ";}
-    if (n%19==0 && n!= 19) {cout <<"19"<< " ";}
-    if (n%23==0 && n!= 23) {cout <<"23"<< " ";}
-    if (n%29==0 && n!= 29) {cout <<"29"<< " ";}
-    if (n%31==0 && n!= 31) {cout <<"31"<< " ";}
-  if (n%47==0 && n!= 47) {cout <<"47"<< " ";}
-    
-
-  cout << endl <<endl;
-
-  return 0;}
-  
+int main()
+{
+int prime_num[11] = { 2,3,5,7,11,13,17,19,23,29,31 };
+int num, iter, c;
+cin >> num;
+c = 0;
+for (iter = 0; iter<11; iter++){
+if (num%prime_num[iter] != 0 || num==prime_num[iter]){
+c++;
+}}
+if (c == 11)
+cout << "Number " << num <<" is prime!"<< endl;
+else
+{
+for (iter = 0; iter<11; iter++)
+{
+if (num%prime_num[iter] == 0)
+cout << prime_num[iter] << " "<< endl;
+}
+cout << "\n" << endl;
+}
+return 0;
+  }
